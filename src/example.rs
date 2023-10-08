6b86fab2d156ba70a8f1870b33a4a9e3aa671e42
Maxime Bouillot <maxime.bouillot@withings.com>
2023-10-09 00:26:29 +0200

    Add a demo executable.

---
+++ src/example.rs
@@ -0,0 +1,49 @@
+extern crate math_parse;
+
+/* ---------------------------------- main ---------------------------------- */
+
+fn main() {
+    let args = concat_cli_arg();
+
+    match math_parse::math_parse_int(&args, None) {
+        Ok(i) => {
+            println!("{i}");
+            std::process::exit(0);
+        },
+        Err(math_parse::MathParseErrors::ReturnFloatExpectedInt(_)) => {
+            // We will proceed to the next match statement that handles floats.
+        },
+        Err(x) => {
+            println!("{x}");
+            std::process::exit(1);
+        },
+    }
+
+    match math_parse::math_parse_float(&args, None) {
+        Ok(f) => {
+            println!("{f}");
+            std::process::exit(0);
+        },
+        Err(x) => {
+            println!("{x}");
+            std::process::exit(1);
+        },
+    }
+}
+
+/* ---------------------------- Helper functions ---------------------------- */
+
+/// Reads the command line arguments and concatenate them on a single string.
+fn concat_cli_arg() -> String {
+    let mut ret = String::new();
+    let mut first_arg = true;
+    for arg in std::env::args() {
+        if first_arg {
+            first_arg = false;
+        } else {
+            ret.push_str(&arg);
+        }
+    }
+    ret
+}
+
