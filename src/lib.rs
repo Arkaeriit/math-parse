9a33843449b3f0193c662609ab09936ae8207418
Maxime Bouillot <maxime.bouillot@withings.com>
2023-10-09 00:23:41 +0200

    Better API.
    A float or an int can be returned.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,26 @@
 /// Module containing the function to parse math expressions.
 mod maths;
+use maths::*;
 
 use std::collections::HashMap;
 
 /// Parse the expression given and apply the optional map of variable that maps
-/// variables to math expressions.
-pub fn math_parse(expression: &str, variable_map: Option<&HashMap<String, String>>) -> Result<i64, MathParseErrors> {
-    if let maths::Number::Int(ret) = maths::math_compute(expression, variable_map)? {
-        Ok(ret)
-    } else {
-        todo!("Error for floats.");
+/// variables to math expressions. Return an integer or error out if the result
+/// is a floating point number.
+pub fn math_parse_int(expression: &str, variable_map: Option<&HashMap<String, String>>) -> Result<i64, MathParseErrors> {
+    match math_compute(expression, variable_map)? {
+        Number::Int(i)   => Ok(i),
+        Number::Float(f) => Err(ReturnFloatExpectedInt(f)),
+    }
+}
+
+/// Parse the expression given and apply the optional map of variable that maps
+/// variables to math expressions. Return a floating point number. If the result
+/// is an integer, convert it as a floating point number.
+pub fn math_parse_float(expression: &str, variable_map: Option<&HashMap<String, String>>) -> Result<f64, MathParseErrors> {
+    match math_compute(expression, variable_map)? {
+        Number::Float(f) => Ok(f),
+        Number::Int(i)   => Ok(i as f64),
     }
 }
 
@@ -45,6 +56,9 @@ pub enum MathParseErrors {
     /// A binary operation have been tried on a float.
     BinaryOpOnFloat(f64, char),
 
+    /// We wanted to return an int but we got a float instead.
+    ReturnFloatExpectedInt(f64),
+
     /// This error should never be raised and should be reported to the
     /// library's maintainer.
     MathParseInternalBug(String),
@@ -64,6 +78,7 @@ impl fmt::Display for MathParseErrors {
             TrailingOperator => write!(f, "An operator is the last element of a line of math."),
             IntConversion(fp) => write!(f, "The floating point number {fp} could not be converted to an int which is needed."),
             BinaryOpOnFloat(fp, c) => write!(f, "The bitwise operation `{c}` is being performed on the floating point number `{fp}`."),
+            ReturnFloatExpectedInt(fp) => write!(f, "An integer was wanted but the floating point number `{fp}` was returned instead."),
             MathParseInternalBug(s) => write!(f, "There is a bug in the math-parse library. The error message is the following:\n{s}\nPlease, report it with the input given to the library to the developer of math-parse over here: https://github.com/Arkaeriit/math-parse"),
         }
     }
